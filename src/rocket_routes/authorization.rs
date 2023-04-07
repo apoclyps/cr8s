1b561fdd6ecccecc17e63da2e2444a6af406c242
apoclyps <kyle90adam@hotmail.com>
2023-04-07 09:51:32 +0100

    set session_id within redis with a TTL of 3 hours

--- src/rocket_routes/authorization.rs
+++ src/rocket_routes/authorization.rs
@@ -1,30 +1,38 @@
 use rocket::{
+    http::Status,
     response::status::Custom,
     serde::json::{json, Json, Value},
 };
-use rocket_db_pools::Connection;
+use rocket_db_pools::{deadpool_redis::redis::AsyncCommands, Connection};
 
-use crate::auth;
 use crate::repositories::UserRepository;
+use crate::{auth, models::User};
 
 use super::{server_error, CacheConn, DbConn};
+use diesel::result::Error::NotFound;
 
 #[post("/login", format = "json", data = "<credentials>")]
 pub async fn login(
     db: DbConn,
-    cache: Connection<CacheConn>,
+    mut cache: Connection<CacheConn>,
     credentials: Json<auth::Credentials>,
 ) -> Result<Value, Custom<Value>> {
-    db.run(move |c| {
-        UserRepository::find_by_username(&c, &credentials.username)
-            .map(|user| {
-                if let Ok(token) = auth::authorize_user(&user, &credentials) {
-                    return json!(token);
-                }
-
-                json!("Unauthorized")
+    let username: String = credentials.username.clone();
+    let user: User = db
+        .run(move |c| {
+            UserRepository::find_by_username(&c, &username).map_err(|e| match e {
+                NotFound => Custom(Status::Unauthorized, json!("Wrong credentials")),
+                _ => server_error(&e.into()),
             })
-            .map_err(|e| server_error(&e.into()))
-    })
-    .await
+        })
+        .await?;
+
+    let session_id: String = auth::authorize_user(&user, &credentials)
+        .map_err(|_| Custom(Status::Unauthorized, json!("Wrong credentials")))?;
+
+    cache
+        .set_ex::<_, _, ()>(format!("sessions/{}", session_id), user.id, 3 * 60 * 60)
+        .await
+        .map(|_| json!({ "token": session_id }))
+        .map_err(|e| server_error(&e.into()))
 }
---
+++ tests/authorization.rs
@@ -0,0 +1,53 @@
+use std::process::Command;
+
+use reqwest::{blocking::Client, StatusCode};
+use serde_json::{json, Value};
+
+pub mod common;
+
+#[test]
+fn test_login() {
+    let output = Command::new("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg("cli")
+        .arg("users")
+        .arg("create")
+        .arg("test_admin")
+        .arg("1234")
+        .arg("admin")
+        .output();
+    println!("{:?}", output);
+
+    let client = Client::new();
+    let response = client
+        .post(format!("{}/login", common::APP_HOST))
+        .json(&json!({
+            "username": "test_admin",
+            "password": "1234",
+        }))
+        .send()
+        .unwrap();
+    
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let json: Value = response.json().unwrap();
+    assert!(json.get("token").is_some());
+    assert_eq!(json["token"].as_str().unwrap().len(), 128);
+}
+
+#[test]
+fn test_login_for_unauthorized_user() {
+    let client = Client::new();
+
+    let response = client
+        .post(format!("{}/login", common::APP_HOST))
+        .json(&json!({
+            "username": "test_admin",
+            "password": "12344",
+        }))
+        .send()
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
