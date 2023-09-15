e1e226642741a4a007e3cee2fea715e390f5383a
apoclyps <kyle90adam@hotmail.com>
2023-09-15 13:22:48 +0100

    configures cors

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -5,12 +5,46 @@ extern crate rocket;
 
 use rocket_db_pools::Database;
 
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+/// Catches all OPTION requests in order to get the CORS related Fairing triggered.
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
         .mount(
             "/",
             routes![
+                all_options,
                 cr8s::rocket_routes::authorization::login,
                 cr8s::rocket_routes::rustaceans::get_rustaceans,
                 cr8s::rocket_routes::rustaceans::view_rustacean,
@@ -24,6 +58,7 @@ async fn main() {
                 cr8s::rocket_routes::crates::delete_crate,
             ],
         )
+        .attach(CORS)
         .attach(cr8s::rocket_routes::DbConn::fairing())
         .attach(cr8s::rocket_routes::CacheConn::init())
         .launch()
